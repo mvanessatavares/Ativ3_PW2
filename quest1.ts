209854b0bb065b76c9680c31aebcc0c58561d25a
Vanessa Lira <vanessa.maria@academico.ifpb.edu.br>
2023-04-15 18:45:52 -0300

    Delete quest1.ts

--- quest1.ts
+++
@@ -1,6 +0,0 @@
-function comparar(a:string[], b:string[]) {
-    return a.length === b.length && a.every((value, index) => value === b[index]);
-}
-
-console.log(comparar(['vanessa'], ['vanessa'])); 
-console.log(comparar(['vanessa'], ['maria'])); 
